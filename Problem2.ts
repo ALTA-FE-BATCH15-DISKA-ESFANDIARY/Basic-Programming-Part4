06fab9c973fe7ec44349ca031fb842d71886a159
Xpow22 <diskaesfandiary22@gmail.com>
2023-07-20 20:20:13 +0700

    menambahkan input biar sesuai sama soal

--- Problem2.ts
+++ Problem2.ts
@@ -7,10 +7,11 @@ function fibonacci(number: number): number {
       return fibonacci(number - 1) + fibonacci(number - 2);
     }
   }
-  
-  console.log(fibonacci(0)); // Output: 0
-  console.log(fibonacci(2)); // Output: 1
-  console.log(fibonacci(9)); // Output: 34
-  console.log(fibonacci(10)); // Output: 55
-  console.log(fibonacci(12)); // Output: 144
+  const deretFibonacci: number[] = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946];
+
+  console.log(fibonacci(0)); 
+  console.log(fibonacci(2)); 
+  console.log(fibonacci(9)); 
+  console.log(fibonacci(10)); 
+  console.log(fibonacci(12)); 
   
